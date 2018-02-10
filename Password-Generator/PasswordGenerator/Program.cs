2d1ba19ca4a8efd1e3ee84b1ad13a818f065824f
Dennis Hernández <dennishernandezvargas@gmail.com>
2018-02-10 11:47:37 -0600

    Initial commit

---
+++ Password-Generator/PasswordGenerator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordGenerator
+{
+    class Program
+    {
+        [STAThreadAttribute]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your password: ");
+            string passwordToConvert = Console.ReadLine();
+
+            string password = Utils.Security.BCrypt.HashPassword(passwordToConvert);
+
+            Console.WriteLine($"Password generated: {password}");
+            Clipboard.SetText(password);
+        }
+    }
+}
